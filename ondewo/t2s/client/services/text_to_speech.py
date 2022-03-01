caddb574c295cea269c4016993a3510c519e6b48
Florencia Cavallin <florenica.cavallin@ondewo.com>
2022-03-01 13:53:18 +0100

    RELEASE 3.1.0

--- ondewo/t2s/client/services/text_to_speech.py
+++ ondewo/t2s/client/services/text_to_speech.py
@@ -1,13 +1,20 @@
+from typing import List
+
 from google.protobuf.empty_pb2 import Empty
 from ondewo.utils.base_services_interface import BaseServicesInterface
 
 from ondewo.t2s.text_to_speech_pb2 import (
     ListT2sPipelinesRequest,
     ListT2sPipelinesResponse,
+    ListT2sLanguagesRequest,
+    ListT2sLanguagesResponse,
+    ListT2sDomainsRequest,
+    ListT2sDomainsResponse,
     SynthesizeRequest,
     SynthesizeResponse,
     T2sPipelineId,
     Text2SpeechConfig,
+    T2SGetServiceInfoResponse,
 )
 from ondewo.t2s.text_to_speech_pb2_grpc import Text2SpeechStub
 
@@ -25,6 +32,10 @@ class Text2Speech(BaseServicesInterface):
         return stub
 
     def synthesize(self, request: SynthesizeRequest) -> SynthesizeResponse:
+        response: SynthesizeResponse = self.stub.Synthesize(request)
+        return response
+
+    def batch_synthesize(self, request: List[SynthesizeRequest]) -> List[SynthesizeResponse]:
         response: SynthesizeResponse = self.stub.Synthesize(request)
         return response
 
@@ -46,4 +57,16 @@ class Text2Speech(BaseServicesInterface):
 
     def list_t2s_pipelines(self, request: ListT2sPipelinesRequest) -> ListT2sPipelinesResponse:
         response: ListT2sPipelinesResponse = self.stub.ListT2sPipelines(request)
+        return response
+
+    def get_service_info(self) -> T2SGetServiceInfoResponse:
+        response: T2SGetServiceInfoResponse = self.stub.GetServiceInfo(request=Empty())
+        return response
+
+    def list_t2s_languages(self, request: ListT2sLanguagesRequest) -> ListT2sLanguagesResponse:
+        response: ListT2sLanguagesResponse = self.stub.ListT2sLanguages(request)
+        return response
+
+    def list_t2s_domains(self, request: ListT2sDomainsRequest) -> ListT2sDomainsResponse:
+        response: ListT2sDomainsRequest = self.stub.ListT2sDomains(request)
         return response
